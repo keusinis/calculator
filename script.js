ca0b37b2784bba0dcf1866c074191637a3b1aa36
keusinis <220072052+keusinis@users.noreply.github.com>
2025-09-19 23:49:28 +0300

    Add button events, no calculations yet

---
+++ script.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll("button");
+const numbers = document.querySelectorAll(".number");
+const screenCurrent = document.querySelector("#screen #current");
+const screenPrev = document.querySelector("#screen #previous");
+const backspace = document.querySelector("#backspace");
+const clear = document.querySelector("#clear");
+const equality = document.querySelector("#equality");
+const operations = document.querySelectorAll(".operation");
+
+let operator = "";
+let prevNumber = 0;
+let currentNumber = 0;
+
+numbers.forEach(button => button.addEventListener("click", (e) => {
+    if(screenCurrent.innerText === "0")
+        screenCurrent.innerText = e.target.innerText;
+    else 
+        screenCurrent.innerText += e.target.innerText;
+}));
+
+backspace.addEventListener("click", () => {
+    screenCurrent.innerText = screenCurrent.innerText.slice(0, -1);
+    if(screenCurrent.innerText === "")
+        screenCurrent.innerText = "0";
+});
+
+clear.addEventListener("click", () => {
+    screenCurrent.innerText = "0";
+});
+
+operations.forEach(button => button.addEventListener("click", (e) => {
+    operator = e.target.textContent;
+    prevNumber = screenCurrent.textContent;
+    screenPrev.innerText = `${prevNumber} ${operator}`;
+}));
+
+equality.addEventListener("click", () => {
+    screenPrev.innerText += ` ${screenCurrent.innerText} =`;
+});
